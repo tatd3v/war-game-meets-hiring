e18c3298a8b61596349e395478238a22bdcb98d7
tatd3v <tatdev91@gmail.com>
2025-04-27 22:04:40 -0500

    add tailwindcss to the project and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     preact(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
