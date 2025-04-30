0cfe7dcdfe3c751c0130c14f62a6864455ac694a
tatd3v <tatdev91@gmail.com>
2025-04-30 15:12:35 -0500

    modify manifest on vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,68 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from "vite-tsconfig-paths";
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     preact(),
     tailwindcss(),
+    tsconfigPaths(),
     VitePWA({
+      injectRegister: 'auto',
       registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
       manifest: {
-        name: 'Galactic Fishing Stats',
-        short_name: 'GalacticStats',
-        description: 'Leaderboard and Market data for the Galactic Fishing game.',
-        theme_color: '#0f172a',
-        background_color: '#0f172a',
-        display: 'standalone',
-        scope: '/',
-        start_url: '/',
-        icons: [
+        "name": "Galactic Fishing Stats",
+        "short_name": "GalacticStats",
+        "description": "Leaderboard and Market data for the Galactic Fishing game.",
+        "theme_color": "#0f172a",
+        "background_color": "#0f172a",
+        "display": "standalone",
+        "start_url": "/",
+        "icons": [
           {
-            src: 'pwa-128x128.png',
-            sizes: '128x128',
-            type: 'image/png',
+            "src": "icons/pwa-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png",
+            "purpose": "maskable any"
           },
           {
-            src: 'pwa-512x512.png',
+            "src": "icons/pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable any"
+          },
+          {
+            src: 'icons/refresh-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'icons/refresh-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-          },
-        ],
+            purpose: 'any'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,woff2}'],
       },
     }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html"
+      }
+    }
+  },
   resolve: {
     alias: {
       'react': 'preact/compat',
